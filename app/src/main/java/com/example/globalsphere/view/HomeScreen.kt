0f7dd6143b6a01bcf9ce6a0c58bf5382044c15f7
jacqui <gitaujaquiline@gmail.com>
2023-08-15 23:10:14 +0300

    Adding screens

--- app/src/main/java/com/example/globalsphere/view/HomeScreen.kt
+++ app/src/main/java/com/example/globalsphere/view/HomeScreen.kt
@@ -1,6 +1,10 @@
 package com.example.globalsphere.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 /**
  * PROJECT NAME: GlobalSphere
@@ -10,6 +14,16 @@ import androidx.compose.runtime.Composable
  * TIME        : 11:15 PM
  */
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    globalSphereState: String, modifier: Modifier = Modifier
+) {
+ResultScreen(globalSphereState, modifier)
+}
+
+@Composable
+fun ResultScreen(countries : String, modifier: Modifier = Modifier) {
+    Box(contentAlignment = Alignment.Center, modifier = modifier) {
+        Text(text = " ")
+    }
 
 }
