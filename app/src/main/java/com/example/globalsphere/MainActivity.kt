8bcbeb7fc0f5127144e98111b7e6927206466c6e
jacqui <gitaujaquiline@gmail.com>
2023-09-19 16:26:03 +0300

    Adding theme

--- app/src/main/java/com/example/globalsphere/MainActivity.kt
+++ app/src/main/java/com/example/globalsphere/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.globalsphere
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.globalsphere.ui.theme.GlobalSphereTheme
 import com.example.globalsphere.view.GlobalSphere
 
@@ -11,8 +16,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GlobalSphereTheme {
-                GlobalSphere()
+                Surface(tonalElevation = 5.dp) {
+                    GlobalSphere()
+                }
             }
         }
+    }
+}
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Composable
+fun GlobalSpherePreview(){
+    GlobalSphereTheme {
+        GlobalSphere()
     }
 }
