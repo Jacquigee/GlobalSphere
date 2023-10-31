2f7766c0de5b39dddb34a9ecb0a2fd4c45f7e019
jacqui <gitaujaquiline@gmail.com>
2023-10-31 23:34:37 +0300

    Adding a sticky header

--- app/src/main/java/com/example/globalsphere/view/HomeScreen.kt
+++ app/src/main/java/com/example/globalsphere/view/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.globalsphere.view
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,14 +14,18 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.globalsphere.R
 import com.example.globalsphere.components.SingleCountryListItem
 import com.example.globalsphere.ui.screens.MainViewModelState
@@ -65,9 +71,22 @@ fun ErrorScreen(modifier: Modifier = Modifier) {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ResultScreen(countries: List<MainViewModelState.Countries>, modifier: Modifier = Modifier, onCountryUpdated: (MainViewModelState.Countries) -> Unit) {
     LazyColumn( verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()){
+        stickyHeader {
+            Text(
+                text = "Global Sphere",
+                style = MaterialTheme.typography.titleMedium,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.DarkGray)
+            )
+        }
         items(countries){
             SingleCountryListItem(country = it){
                 onCountryUpdated(it)
