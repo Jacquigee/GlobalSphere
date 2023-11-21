9e54c9ccb743e00cc5664ed9fb506ba024d26c23
jacqui <gitaujaquiline@gmail.com>
2023-11-21 21:15:31 +0300

    Adding CountrySearchBox component

---
+++ app/src/main/java/com/example/globalsphere/components/SearchButton.kt
@@ -0,0 +1,9 @@
+package com.example.globalsphere.components
+
+/**
+ * PROJECT NAME: GlobalSphere
+ * USER        : jacqui
+ * EMAIL       : gitaujaquiline@gmail.com
+ * DATE        : Nov, 11/21/23
+ * TIME        : 9:13 PM
+ */
